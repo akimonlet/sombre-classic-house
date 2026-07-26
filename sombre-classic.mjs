6810c3a2b751ec9cc4045e470d4b54d6e68bec30
akimonlet <66687014+akimonlet@users.noreply.github.com>
2026-07-26 18:46:18 +0200

    Ajoute sombre-classic.mjs

---
+++ sombre-classic.mjs
@@ -0,0 +1,24 @@
+import { SombreVictimData } from "./module/data-models.mjs";
+import { SombreActorSheet } from "./module/actor-sheet.mjs";
+
+Hooks.once("init", () => {
+  console.log("Sombre Classic — House | Initialisation");
+
+  CONFIG.Actor.dataModels = {
+    victime: SombreVictimData
+  };
+
+  CONFIG.Actor.trackableAttributes = {
+    victime: {
+      bar: ["resources.body", "resources.spirit"],
+      value: ["resources.adrenaline"]
+    }
+  };
+
+  Actors.unregisterSheet("core", ActorSheet, { types: ["victime"] });
+  Actors.registerSheet("sombre-classic-house", SombreActorSheet, {
+    types: ["victime"],
+    makeDefault: true,
+    label: "Fiche Sombre Classic"
+  });
+});
