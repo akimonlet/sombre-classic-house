f90c2704b1cd673ef93a7c426d234f42f97783aa
akimonlet <66687014+akimonlet@users.noreply.github.com>
2026-07-26 18:46:23 +0200

    Ajoute module/constants.mjs

---
+++ module/constants.mjs
@@ -0,0 +1,41 @@
+export const PERSONALITIES = [
+  ["Affectueux", "Possessif", "Abusif"],
+  ["Arrogant", "Orgueilleux", "Mégalomane"],
+  ["Brutal", "Cruel", "Sadique"],
+  ["Charmeur", "Débauché", "Pervers"],
+  ["Conformiste", "Réactionnaire", "Puritain"],
+  ["Cynique", "Désabusé", "Malfaisant"],
+  ["Désinvolte", "Sauvage", "Bestial"],
+  ["Discipliné", "Autoritaire", "Tyrannique"],
+  ["Distant", "Insensible", "Misanthrope"],
+  ["Docile", "Soumis", "Servile"],
+  ["Égocentrique", "Narcissique", "Individualiste"],
+  ["Excentrique", "Illuminé", "Mystique"],
+  ["Fragile", "Maladif", "Morbide"],
+  ["Impulsif", "Téméraire", "Suicidaire"],
+  ["Irritable", "Aigri", "Haineux"],
+  ["Mélancolique", "Défaitiste", "Dépressif"],
+  ["Méthodique", "Maniaque", "Obsessionnel"],
+  ["Nerveux", "Agité", "Frénétique"],
+  ["Paresseux", "Passif", "Masochiste"],
+  ["Passionné", "Acharné", "Fanatique"],
+  ["Prudent", "Méfiant", "Paranoïaque"],
+  ["Rebelle", "Révolté", "Asocial"],
+  ["Rusé", "Sournois", "Diabolique"],
+  ["Timide", "Anxieux", "Angoissé"]
+];
+
+// Sélection compatible avec les restrictions de House of the Rising Dead.
+export const ADVANTAGES = [
+  "Ambidextre", "Atypique", "Bagarreur", "Chef", "Dernier souffle",
+  "Endurci", "Faveur", "Folie douce", "Fort", "In extremis",
+  "Irrésistible", "Lascar", "Lucidité", "Miraculé", "Notable",
+  "Porte-bonheur", "Rêve lucide", "Tir", "Vétéran", "Vigilant"
+];
+
+export const DISADVANTAGES = [
+  "Amnésique", "Bipolaire", "Cardiaque", "Cauchemars", "Chagrin",
+  "Chétif", "Code de conduite", "Dévoué", "Dissocié", "Drogué",
+  "Écervelé", "Ennemi mortel", "Inapte", "Invalide", "Maladroit",
+  "Panique", "Panne", "Somnambule", "Trauma", "Vieux"
+];
